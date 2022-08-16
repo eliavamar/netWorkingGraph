0dfadd671859a3fb8e9d3a2574afaacb3c571e43
shiloa3@gmail.com <shiloa3@gmail.com>
2022-08-16 23:00:44 +0300

    change create graph win

--- gui.py
+++ gui.py
@@ -16,14 +16,17 @@ class gui:
         lblWel = Label(self.master, text='Welcome To Project')
         lblWel.place(x=150, y=40)
 
-        self.lblLoad = Label(self.master, text='Path CSV File')
-        self.lblLoad.place(x=100, y=80)
+        lblLoad = Label(self.master, text='File Loaded!' )
+        self.lblPath = Label(self.master, text='Path CSV File')
+        self.lblPath.place(x=100, y=80)
         self.pathString = StringVar()
+
+
         self.pathEn = Entry(width=23, textvariable=self.pathString)
         self.pathEn.place(x=180, y=80)
         self.loadBut = Button(self.master,
                               text='Load CSV File',
-                              command=lambda: self.loadCsv(self.pathString.get()),
+                              command=lambda: (self.loadCsv(self.pathString.get()), lblLoad.place(x=165, y=148)),
                               width=15)
         self.loadBut.place(x=154, y=120)
 
@@ -31,20 +34,20 @@ class gui:
                                text='Build Graph',
                                command=self.buildGraphWindow,
                                width=20)
-        self.buildBut.place(x=140, y=170)
+        self.buildBut.place(x=140, y=200)
 
         self.mlBut = Button(self.master,
                             text='ML Algorithm',
                             command=self.mlAlgorithemWindow,
                             width=20)
-        self.mlBut.place(x=140, y=200)
+        self.mlBut.place(x=140, y=230)
 
         mainloop()
 
     def mlAlgorithemWindow(self):
         buildAlgo = Toplevel(self.master)
         buildAlgo.title("ML Graph")
-        buildAlgo.geometry("400x380")
+        buildAlgo.geometry("400x340")
         lbCcol = Label(buildAlgo, text='col')
         lbAlgo = Label(buildAlgo, text='algorithm')
         lbOut = Label(buildAlgo, text='out file')
@@ -78,7 +81,7 @@ class gui:
         self.loadCsv(self.pathString.get())
         buidGraphWin = Toplevel(self.master)
         buidGraphWin.title("Build Graph")
-        buidGraphWin.geometry("400x450")
+        buidGraphWin.geometry("400x340")
 
         lbNeo = Label(buidGraphWin, text='Neo4j Configuration')
         lbNeo.place(x=140, y=25)
@@ -108,14 +111,14 @@ class gui:
         lbSrc = Label(buidGraphWin, text='Src')
         lbDest = Label(buidGraphWin, text='Dest')
         lblWei = Label(buidGraphWin, text='Weight')
-        lblGro = Label(buidGraphWin, text='Group By')
-        lblAgg = Label(buidGraphWin, text='Select Agg')
+        # lblGro = Label(buidGraphWin, text='Group By')
+        # lblAgg = Label(buidGraphWin, text='Select Agg')
 
         lbSrc.place(x=100, y=180)
         lbDest.place(x=100, y=210)
         lblWei.place(x=100, y=240)
-        lblGro.place(x=100, y=270)
-        lblAgg.place(x=100, y=340)
+        # lblGro.place(x=100, y=270)
+        # lblAgg.place(x=100, y=340)
 
         srcStr = StringVar()
         destStr = StringVar()
@@ -131,34 +134,39 @@ class gui:
         comboDest.place(x=200, y=210)
         comboWei.place(x=200, y=240)
 
-        listGroup = Listbox(buidGraphWin, selectmode="multiple", height=3, width=20, exportselection=False)
-        listGroup.pack(padx=5, pady=5,
-                       expand=YES)
-        listGroup.place(x=200, y=270)
-
-        update = Button(buidGraphWin,
-                        text='update',
-                        command=lambda: (listGroup.delete(0, END), listGroup.insert(END, srcStr.get()),
-                                         listGroup.insert(END, destStr.get()), listGroup.insert(END, weiStr.get())),
-                        width=7)
-        update.place(x=30, y=280)
-
-        listAggr = Listbox(buidGraphWin, selectmode="multiple", height=3, width=20, exportselection=False)
-        listAggr.curselection()
-        listAggr.pack(padx=5, pady=5,
-                      expand=YES)
-        for item in agg_function_list():
-            listAggr.insert(END, item)
-        listAggr.place(x=200, y=340)
+        # listGroup = Listbox(buidGraphWin, selectmode="multiple", height=3, width=20, exportselection=False)
+        # listGroup.pack(padx=5, pady=5,
+        #                expand=YES)
+        # listGroup.place(x=200, y=270)
+        #
+        # update = Button(buidGraphWin,
+        #                 text='update',
+        #                 command=lambda: (listGroup.delete(0, END), listGroup.insert(END, srcStr.get()),
+        #                                  listGroup.insert(END, destStr.get()), listGroup.insert(END, weiStr.get())),
+        #                 width=7)
+        # update.place(x=30, y=280)
+        #
+        # listAggr = Listbox(buidGraphWin, selectmode="multiple", height=3, width=20, exportselection=False)
+        # listAggr.curselection()
+        # listAggr.pack(padx=5, pady=5,
+        #               expand=YES)
+        # for item in agg_function_list():
+        #     listAggr.insert(END, item)
+        # listAggr.place(x=200, y=340)
+        # runGraph = Button(buidGraphWin,
+        #                   text='Run',
+        #                   command=lambda: self.createGraph(urlString.get(), userString.get(), passString.get(),
+        #                                                    srcStr.get(),
+        #                                                    destStr.get(), weiStr.get(),
+        #                                                    [listGroup.get(i) for i in listGroup.curselection()],
+        #                                                    [listAggr.get(i) for i in listAggr.curselection()]),
+        #                   width=20)
         runGraph = Button(buidGraphWin,
                           text='Run',
                           command=lambda: self.createGraph(urlString.get(), userString.get(), passString.get(),
                                                            srcStr.get(),
-                                                           destStr.get(), weiStr.get(),
-                                                           [listGroup.get(i) for i in listGroup.curselection()],
-                                                           [listAggr.get(i) for i in listAggr.curselection()]),
-                          width=20)
-        runGraph.place(x=140, y=420)
+                                                           destStr.get(), weiStr.get()),width=20)
+        runGraph.place(x=140, y=300)
 
     def createGraph(self, url, user, password, src, dest, w, groupList=None, aggList=None):
         if w == '': w=None
@@ -175,7 +183,7 @@ class gui:
             self.drawHtmlGraph(self.csv, url, user, password, src, dest, w)
 
     def drawHtmlGraph(self, df, url, user, password, src, dest, w):
-        G = Graph(df, url, user, password, src, dest, w)
+        G = Graph(self.csv.get_df(), url, user, password, src, dest, w)
         # G.draw_graph(len(df))
 
     def playAlgorithems(self):
@@ -183,25 +191,4 @@ class gui:
 
     def loadCsv(self, csvFile):
         self.csv = Csv(csvFile)
-    #
-    #     buildBut = Button(newWindow,
-    #                       text='Create Graph',
-    #                       command=self.openConnWindow,
-    #                       width=30)
-    #     b1.place(x=110, y=330)
-    #
-    #     self.n = StringVar()
-    #     self.n2 = StringVar()
-    #     self.n3 = StringVar()
-    #     self.t2 = Combobox(self.master, width=20, textvariable=self.n)
-    #     self.t2['values'] = (self.csv.get_columns())
-    #     self.t3 = Combobox(self.master, width=20, textvariable=self.n2)
-    #     self.t3['values'] = (self.csv.get_columns())
-    #     self.t4 = Combobox(self.master, width=20, textvariable=self.n3)
-    #     self.t4['values'] = (self.csv.get_columns())
-    #     self.lbl2.place(x=100, y=150)
-    #     self.t2.place(x=200, y=150)
-    #     self.lbl3.place(x=100, y=200)
-    #     self.t3.place(x=200, y=200)
-    #     self.lbl4.place(x=100, y=250)
-    #     self.t4.place(x=200, y=250)
+
